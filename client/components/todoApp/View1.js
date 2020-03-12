fe0dbd7b9d6978c8bf0d61ce44d86b49f5a99658
Riku Heikkinen <riku.heikkinen94@gmail.com>
2020-03-12 18:45:43 +0200

    Some styling for the home screen and refactoring of code

--- client/components/todoApp/View1.js
+++ client/components/todoApp/View1.js
@@ -1,27 +1,71 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, StyleSheet, TouchableOpacity } from "react-native";
 
 const View1 = props => {
   console.log("these are the props", props.todos);
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>You have successfully logged in</Text>
+    <View style={styles.screen}>
       {props.todos.map(t => (
-        <Text key={t.id}>
-          {t.title}
-          <Text>{t.category}</Text>
-          <Text>{t.city}</Text>
-          <Text>{t.price}</Text>
-          <Text>{t.id}</Text>
-        </Text>
+        <TouchableOpacity
+          onPress={() => props.navigation.navigate("Post", { postID: t.id })}
+        >
+          <Text style={styles.textTitle} key={t.id}>
+            {t.title + "\n"}
+            <Text style={styles.text}>{t.category + "\n"}</Text>
+            <Text style={styles.text}>{t.city + "\n"}</Text>
+            <Text style={styles.text}>{t.price + "€"}</Text>
+            <Text style={styles.text}>{t.id + "\n"}</Text>
+          </Text>
+        </TouchableOpacity>
       ))}
+      <TouchableOpacity onPress={props.onLogout}>
+        <View style={styles.primaryButton}>
+          <Text style={styles.primaryButtonText}>Logout</Text>
+        </View>
+      </TouchableOpacity>
       {/* <Button
         title="Go to Todo List View"
         onPress={() => props.navigation.navigate("Todos")}
       /> */}
-      <Button title="Logout" onPress={props.onLogout} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  screen: {
+    backgroundColor: "rgb(50, 50, 50)",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  header: {
+    fontSize: 40,
+    marginBottom: 20,
+    color: "white"
+  },
+  text: {
+    fontSize: 20,
+    color: "white"
+  },
+  textTitle: {
+    fontSize: 30,
+    color: "white"
+  },
+  primaryButton: {
+    backgroundColor: "#BF87E8",
+    height: 60,
+    width: 200,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "black",
+    borderWidth: 2,
+    marginTop: 20,
+    marginBottom: 10
+  },
+  primaryButtonText: {
+    color: "white",
+    fontSize: 20
+  }
+});
 
 export default View1;
